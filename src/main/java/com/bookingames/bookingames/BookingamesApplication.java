7333a7feaa895d0340e2cc052f9b47574113618e
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2023-04-17 16:40:44 +0200

    Create spring project

---
+++ src/main/java/com/bookingames/bookingames/BookingamesApplication.java
@@ -0,0 +1,13 @@
+package com.bookingames.bookingames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingames/bookingames/BookingamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingames.bookingames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
